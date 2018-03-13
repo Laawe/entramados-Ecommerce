59ecaf2edafc1ade59ec2117a1795355d032f8dc
Laawe <laura.render.03@gmail.com>
2018-03-12 19:11:00 -0600

    carrito-botones

--- js/database.js
+++ js/database.js
@@ -3,123 +3,105 @@ entramados = [
         title: "blusa-chiapas1",
         price: 800,
         id: 1,
-        photo: "../img/products/blusa-chiapas1.jpg",
+        photo: "../assets/img/products/blusa-chiapas1.jpg",
         state:"CHIAPAS", 
     },
     {
         title: "bolsa-cdmx",
         price: 300,
         id: 2,
-        photo: "../img/products/bolsa-cdmx.png",
+        photo: "../assets/img/products/bolsa-cdmx.png",
         state:"CDMX", 
     },
     {
         title: "camisa-tenango1",
         price: 600,
         id: 3,
-        photo: "../img/products/camisa-tenango1.jpg",
+        photo: "../assets/img/products/camisa-tenango1.jpg",
         state:"HIDALGO", 
     },
     {
         title: "cartera-cdmx",
         price: 400,
         id: 4,
-        photo: "../img/products/cartera-cdmx.jpg",
+        photo: "../assets/img/products/cartera-cdmx.jpeg",
         state:"CDMX", 
     },
     {
         title: "cartera-cdmx1",
         price: 400,
         id: 5,
-        photo: "../img/products/cartera-cdmx1.jpeg",
+        photo: "../assets/img/products/cartera-cdmx1.jpeg",
         state:"CDMX", 
     },
     {
         title: "cojin-cdmx",
         price: 400,
         id: 6,
-        photo: "../img/products/cojin-cdmx.png",
+        photo: "../assets/img/products/cojin-cdmx.png",
         state:"CDMX", 
     },
     {
         title: "cojin-tenango",
         price: 400,
         id: 7,
-        photo: "../img/products/cojin-tenango.png",
+        photo: "../assets/img/products/cojin-tenango1.jpg",
         state:"HIDALGO", 
     },
     {
         title: "estuche-cdmx1",
         price: 400,
         id: 8,
-        photo: "../img/products/estuche-cdmx1.png",
+        photo: "../assets/img/products/estuche-cdmx1.png",
         state:"CDMX", 
     },
     {
         title: "estuche-cdmx5",
         price: 400,
         id: 9,
-        photo: "../img/products/estuche-cdmx5.png",
+        photo: "../assets/img/products/estuche-cdmx5.png",
         state:"CDMX", 
     },
     {
         title: "estuche-cdmx7",
         price: 400,
         id: 10,
-        photo: "../img/products/estuche-cdmx7.png",
+        photo: "../assets/img/products/estuche-cdmx7.png",
         state:"CDMX", 
     },
     {
         title: "funda-cdmx",
         price: 400,
         id: 11,
-        photo: "../img/products/funda-cdmx.png",
+        photo: "../assets/img/products/funda-cdmx.png",
         state:"CDMX", 
     },
     {
         title: "funda-cdmx1",
         price: 400,
         id: 12,
-        photo: "../img/products/funda-cdmx.png1",
+        photo: "../assets/img/products/funda-cdmx1.png",
         state:"CDMX", 
     },
     {
         title: "rebozo-cdmx",
         price: 700,
         id: 13,
-        photo: "../img/products/rebozo-cdmx.png",
+        photo: "../assets/img/products/rebozo-cdmx.png",
         state:"CDMX", 
     },
     {
         title: "rebozo-cdmx1",
         price: 700,
         id: 14,
-        photo: "../img/products/rebozo-cdmx1.png",
+        photo: "../assets/img/products/rebozo-cdmx1.jpg",
         state:"CDMX", 
     },
     {
         title: "sarape-cdmx",
         price: 900,
         id: 15,
-        photo: "../img/products/sarape-cdmx.png",
+        photo: "../assets/img/products/sarape-cdmx.png",
         state:"CDMX", 
     },
 ]
-
-
-/*template = "" ;
-let row = document.getElementById("")
-entramados.forEach(product => {
-    template += `
-    <div class="4u 12u(mobile)">
-        <section>
-		    <a href="#" class="image featured"><img src="${product.photo}" alt="" /></a>
-				<header>
-					<h3>${product.title}</h3>
-				</header>
-					<p><strong>Precio: </strong>${product.price}</p>
-        </section>
-	</div>
-    `  
-});
-row.innerHTML = template;*/
